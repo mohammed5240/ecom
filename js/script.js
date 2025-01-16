e3a7d5c168b77a4cb118cf57b31db13c6f131f67
amine <mehemmedamineelbouziani969@gmail.com>
2025-01-16 15:09:15 +0000

    create slider

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
-    const sidebarLinks = document.querySelectorAll('.sidebar ul li a');
+    // const sidebarLinks = document.querySelectorAll('.sidebar ul li a');
 
-    // Ajouter un écouteur d'événement sur chaque lien
-    sidebarLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            // Supprimez la classe active de tous les liens
-            sidebarLinks.forEach(link => link.classList.remove('active'));
+    // // Ajouter un écouteur d'événement sur chaque lien
+    // sidebarLinks.forEach(link => {
+    //     link.addEventListener('click', (event) => {
+    //         // Supprimez la classe active de tous les liens
+    //         sidebarLinks.forEach(link => link.classList.remove('active'));
             
-            // Ajoutez la classe active au lien cliqué
-            event.target.classList.add('active');
-        });
-    });
+    //         // Ajoutez la classe active au lien cliqué
+    //         event.target.classList.add('active');
+    //     });
+    // });
     
 
     
 
 
+const productContainers = [...document.querySelectorAll('.slider .content_image ul')];
+const btn1 = [...document.querySelectorAll('.slider .content_image .btn1')];
+const btn2 = [...document.querySelectorAll('.slider .content_image .btn2')];
+
+productContainers.forEach((item,i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    console.log(containerDimensions);
+    let containerWidth = containerDimensions.width;
+    console.log(containerWidth);
+    btn1[i].addEventListener('click',() => {
+        item.scrollLeft -= containerWidth;
+        console.log(btn1[i])
+    })
+    btn2[i].addEventListener('click',() => {
+        item.scrollLeft += containerWidth;
+    })
+        
+    })
